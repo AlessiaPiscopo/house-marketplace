8292b0e4ccc056ee583023777026a38c470c72a8
Alessia Piscopo <alessia.piscopo@hotmail.com>
2022-05-22 13:39:51 -0400

    ceate sign up and sign in components

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,148 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { db } from "../firebase.config";
+import { doc, setDoc, serverTimestamp } from "firebase/firestore";
+
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+
 const SignUp = () => {
-    return (
-        <div>
-            <h1>SignUp</h1>
-        </div>
-    )
-}
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const { name, email, password } = formData;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleChange = (ev) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [ev.target.id]: ev.target.value,
+    }));
+  };
+
+  // authenticate user & add to database
+  const handleSubmit = async (ev) => {
+    ev.preventDefault();
+
+    try {
+      // authentication ------------------------------------
+
+      // see docs: https://firebase.google.com/docs/auth/web/start
+
+      // get auth value from getAuth method
+      const auth = getAuth();
+
+      // createUserWithEmailAndPassword returns a promise
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      // need this for our database
+      const user = userCredential.user;
+
+      // update display name
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+
+      // add to database ----------------------------------
+
+      // see docs for the various ways to add data to Firestore: https://firebase.google.com/docs/firestore/manage-data/add-data
+
+      // addDoc() lets Firebase generate a unique id (used when we don't need a meaningful id for the doc)
+
+      // we use setDoc(), which requires us to explicity set an id, since we need matching user ids in Auth & Firestore
+
+      // since we don't want to change the formData state, we create a copy of the formData object (excluding the password)
+      const formDataCopy = {
+        ...formData,
+      };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
+
+      // redirect to homepage
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back</p>
+        </header>
+
+        <main>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="nameInput"
+              placeholder="Name"
+              id="name"
+              value={name}
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              className="emailInput"
+              placeholder="Email"
+              id="email"
+              value={email}
+              onChange={handleChange}
+            />
+            <div className="passwordInputDiv">
+              <input
+                type={showPassword ? "text" : "password"}
+                className="passwordInput"
+                placeholder="Password"
+                id="password"
+                value={password}
+                onChange={handleChange}
+              />
+              <img
+                src={visibilityIcon}
+                alt="show password"
+                className="showPassword"
+                onClick={() => setShowPassword((prevState) => !prevState)}
+                // onClick={() => setShowPassword(!showPassword)}
+              />
+            </div>
+            <Link to="/forgot-password" className="forgotPasswordLink">
+              Forgot Password?
+            </Link>
+            <div className="signInBar">
+              <p className="signInText">Sign In</p>
+              <button className="signInButton">
+                <ArrowRightIcon fill="#fff" width="34px" height="34px" />
+              </button>
+            </div>
+          </form>
+
+          {/* Google OAuth */}
+
+          <Link to="/sign-in" className="registerLink">
+            Sign In Instead
+          </Link>
+        </main>
+      </div>
+    </>
+  );
+};
 
 export default SignUp;
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,9 +1,87 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+// don't need visibilityIcon as ReactComponent, bcus we are using this as an img src
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+
 const SignIn = () => {
-    return (
-        <div>
-            <h1>SignIn</h1>
-        </div>
-    )
-}
+  const navigate = useNavigate();
+
+  // create a formData object with all input fields (instead of setting the state for each field individually)
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  // then, we can destructure from the formData object to have access to each field
+  const { email, password } = formData;
+  const [showPassword, setShowPassword] = useState(false);
+
+  // dynamically sets the names and values for the new formData object
+  // [ev.target.id] receives the id of "email" or "password" (which we set in the <input> elements below)—this allows us to add as many more inputs as we want without the need to target each individual
+  const handleChange = (ev) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [ev.target.id]: ev.target.value,
+    }));
+  };
+
+  return (
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back</p>
+        </header>
+
+        <main>
+          <form>
+            <input
+              type="email"
+              className="emailInput"
+              placeholder="Email"
+              id="email"
+              value={email}
+              onChange={handleChange}
+            />
+            <div className="passwordInputDiv">
+              <input
+                type={showPassword ? "text" : "password"}
+                className="passwordInput"
+                placeholder="Password"
+                id="password"
+                value={password}
+                onChange={handleChange}
+              />
+
+              <img
+                src={visibilityIcon}
+                alt="show password"
+                className="showPassword"
+                onClick={() => setShowPassword((prevState) => !prevState)}
+                // onClick={() => setShowPassword(!showPassword)}
+              />
+            </div>
+
+            <Link to="/forgot-password" className="forgotPasswordLink">
+              Forgot Password?
+            </Link>
+
+            <div className="signInBar">
+              <p className="signInText">Sign In</p>
+              <button className="signInButton">
+                <ArrowRightIcon fill="#fff" width="34px" height="34px" />
+              </button>
+            </div>
+          </form>
+
+          {/* Google OAuth */}
+
+          <Link to="/sign-up" className="registerLink">
+            Sign Up Instead
+          </Link>
+        </main>
+      </div>
+    </>
+  );
+};
 
 export default SignIn;
