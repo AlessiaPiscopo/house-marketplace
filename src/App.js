90faee3722023168fd917a5717d967bc894271a2
Alessia Piscopo <alessia.piscopo@hotmail.com>
2022-05-21 22:50:58 -0400

    create navbar component, add functionality and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+
 // pages
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
@@ -20,8 +22,8 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
+        <Navbar />
       </Router>
-      {/* Navbar */}
     </>
   );
 }
